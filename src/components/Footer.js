eda9bba5eb7435a8dc03f093b3f1176b28c0595b
Francisca Estefanía Martínez Lozano <fany.marloz@gmail.com>
2022-05-25 02:38:40 -0500

    Cambios pokedex

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por Estefania ML <a href="https://gitlab.com/EstefaniaML">gitlab: EstefaniaML</a>
+     </div>
+    <div>
+    <a href="https://github.com/EstefaniaMarloz"> github: EstefaniaMarloz</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,63 @@
+import React, { useContext } from "react";
+import FavoriteContext from "../contexts/favoritesContext";
+
+const Pokemon = (props) => {
+  const { pokemon } = props;
+  const { favoritePokemons, updateFavoritePokemons } = useContext(
+    FavoriteContext
+  );
+
+  const redHeart = "❤️";
+  const blackHeart = "🖤";
+  const heart = favoritePokemons.includes(pokemon.name) ? redHeart : blackHeart;
+
+  const clickHeart = (e) => {
+    e.preventDefault();
+    updateFavoritePokemons(pokemon.name);
+  };
+
+  return (
+    <div className="pokemon-card">
+      <div className="pokemon-img-container">
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          className="pokemon-img"
+        />
+      </div>
+      <div className="card-body">
+        <div className="card-top">
+          <h3>{pokemon.name}</h3>
+          <div>#{pokemon.id}</div>
+        </div>
+        <div className="card-bottom">
+          <div className="pokemon-type">
+            {pokemon.types.map((type, idx) => {
+              return ( 
+                <div key={idx} className="pokemon-type-text">
+                  {type.type.name}
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="pokemon-stat">
+            {pokemon.stats.map((stat, idx) => {
+              return (
+                <div key={idx} className="pokemon-stat-text">
+                  {stat.stat.name} <n>{stat.base_stat}</n>
+                </div>
+              );
+            })}
+          </div>
+
+          <button onClick={clickHeart} className="pokemon-heart-btn">
+            <div className="pokemon-favorite">{heart}</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pokemon;
